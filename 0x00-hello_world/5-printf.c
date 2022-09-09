97331ea883e954cb63405176d1e809803b8666fa
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-09 05:49:27 -0700

    hello world: task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,12 +5,11 @@
  *
  * Description - 'A program to out a sequence of string'
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
