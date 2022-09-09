8f916f0f4c66f86932b67f910219f3fa88e6d281
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-09 05:02:10 -0700

    hello world: task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
-/*
+/**
  *
- * Printing a string using puts
+ * main - Entry point
  *
  *
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
