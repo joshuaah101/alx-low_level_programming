d04e28b71f200639d6ffc717e64bb4a9ac6ea353
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-10 15:04:38 -0700

    Variables if else: task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'Z'; a <= 'A'; a--)
+	{
+		char x = tolower(a);
+
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+
+}
