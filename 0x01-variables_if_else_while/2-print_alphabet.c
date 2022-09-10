c014ae693e374eff522af6a92dac8f241d3b05d5
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-09 18:08:44 -0700

    Variables if else: task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		char x = tolower(a);
+		putchar(x);
+	}	
+	return (0);
+
+}
