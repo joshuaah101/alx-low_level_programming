aca5b846e6ccb9446d9a76f32d7673d9c09fc461
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-09 18:58:38 -0700

    Variables if else: task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
