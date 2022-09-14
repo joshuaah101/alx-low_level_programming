138778008eefe5e9ffaf3785d6ce91139e5791e8
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-14 12:43:10 -0700

    functions nested loop: task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print 10 times lowercase alphabets
+ *
+ * Return: always 0 
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
