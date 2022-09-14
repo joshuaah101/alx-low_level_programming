db331575de849745453cc0fc563e5bfe757f69b4
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-14 10:57:58 -0700

    functions nested if: task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-int main()
+/**
+ * main - to print _putchar to stdout
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, size = sizeof(str) / sizeof(int);
+	
+	for (count = 0; count < size; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef _main_h
+#define main_h
+
+int _putchar(char c);
+#endif
