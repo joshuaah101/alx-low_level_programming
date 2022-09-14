48be0a86a01e7ddcc704309da372da69f5f10e7c
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-14 16:37:29 -0700

    functions nested loop: task 10

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89,9);
+	printf("%d\n", n);
+
+	return (0);
+}
