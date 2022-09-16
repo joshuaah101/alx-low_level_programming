1f4579a1133f571ccde0c687c242e58efd9f4cc6
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-15 23:51:53 -0700

    Debugging: task 3

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month = 02, day = 29, year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
