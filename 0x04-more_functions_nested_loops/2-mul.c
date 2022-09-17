9c6419f8827de0496d366668355c9402d5de7867
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-16 18:43:58 -0700

    more functions: task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - a function that finds the product of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
