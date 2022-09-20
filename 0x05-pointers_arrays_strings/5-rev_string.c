535cfa9bfeef5fe97d0e0bcf22a63865d4f81def
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-19 21:51:40 -0700

    Arrays pointers and strings: task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,19 @@
 
 void rev_string(char *s)
 {
-	int size = strlen(s) - 1, i;
+	int len, i, half;
+	char temp;
 
-	for (i = size; i >= 0; i--)
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
 	{
-		printf("%c", s[i]);
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
 	}
 }
