2b9c7e24448282aca2eb8505b3d3c5c7f294b0fd
Joshua Oluwajare Faloye <faloyejoshua@gmail.com>
2022-09-19 20:58:24 -0700

    Arrays pointers and strings: task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - the function to test
+ * @str: first the parameter to test
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i =0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
